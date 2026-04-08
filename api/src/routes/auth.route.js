85af3aaeb7bfef619387c8c3c04bc84167ebf525
murthehelp24 <attachaijumpahom@gmail.com>
2026-04-09 00:51:19 +0700

    TEST LOGIN

--- api/src/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { auth} from '../../auth.js'
-
-const router = express.Router();
-
-router.get('/login', )
-
-export default router
---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
+
+function App() {
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-md px-4">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow-lg">
+              <li><a>Item 1</a></li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
+                </ul>
+              </li>
+              <li><a>Item 3</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl font-bold tracking-tight">daisyUI</a>
+        </div>
+
+        <div className="navbar-center hidden lg:flex">
+        </div>
+
+        <div className="navbar-end gap-2">
+          <Show when="signed-out">
+            <SignInButton>
+              <button className="btn btn-ghost btn-sm md:btn-md">เข้าสู่ระบบ</button>
+            </SignInButton>
+            <SignUpButton>
+              <button className="btn btn-primary btn-sm md:btn-md shadow-sm">สมัครสมาชิก</button>
+            </SignUpButton>
+          </Show>
+
+          <Show when="signed-in">
+            <div className="flex items-center gap-3">
+              <div className="avatar">
+                <UserButton afterSignOutUrl="/" />
+              </div>
+            </div>
+          </Show>
+        </div>
+      </div>
+    </>
+
+  )
+}
+
+export default App
